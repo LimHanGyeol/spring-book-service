8c629b21586614109ace92a2325f8fd161735afe
LimHanGyeol <dlagksruf19@naver.com>
2020-04-03 00:39:25 +0900

    Project Init

---
+++ src/main/java/me/hangyeol/springbookservice/SpringBookServiceApplication.java
@@ -0,0 +1,13 @@
+package me.hangyeol.springbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookServiceApplication.class, args);
+    }
+
+}
