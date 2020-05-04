8312907e8094e59cff87060980cb5925c5185cb8
LimHanGyeol <dlagksruf19@naver.com>
2020-05-04 20:00:24 +0900

    IoC 컨테이너와 빈 - Bean Scope

---
+++ src/main/java/me/hangyeol/springbookservice/scope/Single.java
@@ -0,0 +1,17 @@
+package me.hangyeol.springbookservice.scope;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Single {
+
+    @Autowired
+    private ObjectProvider<Proto> proto;
+
+    public Proto getProto() {
+        return proto.getIfAvailable();
+    }
+}
