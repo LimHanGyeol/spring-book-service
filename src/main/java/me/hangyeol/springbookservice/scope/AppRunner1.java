21265368c86810115558faa377e742bc7824c5d4
LimHanGyeol <dlagksruf19@naver.com>
2020-05-05 00:39:44 +0900

    section1 - Environment Profile

--- src/main/java/me/hangyeol/springbookservice/scope/AppRunner1.java
+++ src/main/java/me/hangyeol/springbookservice/scope/AppRunner1.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AppRunner implements ApplicationRunner {
+public class AppRunner1 implements ApplicationRunner {
 
     /*
     @Autowired
