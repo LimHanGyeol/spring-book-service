2a2a248f9eebf5072c7b4292cd93705551afd08c
LimHanGyeol <dlagksruf19@naver.com>
2020-05-06 15:26:18 +0900

    section2 - Resource/Validation : Resource 추상화

--- src/main/java/me/hangyeol/springbookservice/resourceloader/AppRunner5.java
+++ src/main/java/me/hangyeol/springbookservice/resourceloader/AppRunner5.java
@@ -3,6 +3,7 @@ package me.hangyeol.springbookservice.resourceloader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,16 @@ import java.nio.file.Files;
 public class AppRunner5 implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("=== resourceLoader");
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
+
+        System.out.println(resourceLoader.getClass());
+
+        Resource resource = resourceLoader.getResource("test.txt");
+        System.out.println(resource.getClass());
         System.out.println(resource.exists());
         System.out.println();
         System.out.println(resource.getDescription());
