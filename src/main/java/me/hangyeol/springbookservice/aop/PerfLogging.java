f2183067a4994ce8426f9fd85d51adb79dabaaff
LimHanGyeol <dlagksruf19@naver.com>
2020-05-10 15:38:16 +0900

    section5 - Spring AOP : 어노테이션기반의 @AOP

---
+++ src/main/java/me/hangyeol/springbookservice/aop/PerfLogging.java
@@ -0,0 +1,12 @@
+package me.hangyeol.springbookservice.aop;
+
+import java.lang.annotation.*;
+
+/**
+ * 이 어노테이션을 사용하면 성능을 로깅해 줍니다.
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface PerfLogging {
+}
